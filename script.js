950a25d908d06a1246b1f88359f9f0cc1669ad2b
luka <lukakapanadze04@gmail.com>
2024-01-22 22:36:29 +0400

    added sticky nav and updated learn more section

--- script.js
+++ script.js
@@ -10,32 +10,40 @@ const box = document.querySelector(".box5");
 const img = document.querySelector(".moreinfoimg");
 const img1 = document.querySelector(".moreinfoimg1");
 const img2 = document.querySelector(".moreinfoimg2");
-
+///read more
 btn1.addEventListener("click", function () {
   text1.classList.toggle("hidden");
-
+  img1.classList.remove("rotate180");
+  img2.classList.remove("rotate180");
   img.classList.toggle("rotate180");
+  text2.classList.add("hidden");
+  text3.classList.add("hidden");
 });
 btn2.addEventListener("click", function () {
   text2.classList.toggle("hidden");
   img1.classList.toggle("rotate180");
+  img.classList.remove("rotate180");
+  img2.classList.remove("rotate180");
+  text1.classList.add("hidden");
+  text3.classList.add("hidden");
 });
 btn3.addEventListener("click", function () {
   text3.classList.toggle("hidden");
   img2.classList.toggle("rotate180");
+  img.classList.remove("rotate180");
+  img1.classList.remove("rotate180");
+  text1.classList.add("hidden");
+  text2.classList.add("hidden");
 });
-
-const nav = document.querySelector(".navigation");
-const main = document.querySelector(".courses");
-const stickyNav = function (entries) {
-  const [entry] = entries;
-  console.log(entry);
-  if (entry.Intersecting) nav.classList.add("sticky-color");
-  else nav.classList.remove("sticky-color");
-};
-
-const navObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0.02,
+// stikcy navigaction
+document.addEventListener("scroll", function () {
+  const nav = document.querySelector(".navigation");
+  const team = document.querySelector(".teammembers");
+  if (window.scrollY > 0) {
+    nav.classList.add("sticky");
+    team.classList.add("sticky2");
+  } else {
+    nav.classList.remove("sticky");
+    team.classList.remove("sticky2");
+  }
 });
-navObserver.observe(main);

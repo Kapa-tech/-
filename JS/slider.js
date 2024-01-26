05a8b3fa37972d69f0539fb2b7aa7947ffb9fb7d
luka <lukakapanadze04@gmail.com>
2024-01-26 18:11:07 +0400

    y

--- JS/slider.js
+++ JS/slider.js
@@ -1,53 +1,6 @@
 "use strict";
 
-// const slides = document.querySelectorAll(".slide");
-
-// let curSlide = 0;
-
-// const maxSlide = slides.length;
-// const btnLeft = document.querySelector(".slide-arrow-l");
-// const btnRight = document.querySelector(".slide-arrow-r");
 const dotContainer = document.querySelector(".dots");
-// const dot = document.querySelector(".dots-dot");
-// const dot2 = document.querySelector(".dots-dot1");
-// const dot3 = document.querySelector(".dots-dot2");
-
-// const goToSlide = function (slide) {
-//   slides.forEach(
-//     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-//   );
-// };
-
-// const nextSlide = function () {
-//   if (curSlide === maxSlide - 1) {
-//     curSlide = 0;
-//   } else {
-//     curSlide++;
-//   }
-//   goToSlide(curSlide);
-//   // activateDot(curSlide);
-// };
-
-// const prevSlide = function () {
-//   if (curSlide === 0) {
-//     curSlide = maxSlide - 1;
-//   } else {
-//     curSlide--;
-//   }
-
-//   goToSlide(curSlide);
-// };
-
-// goToSlide(0);
-
-// btnRight.addEventListener("click", nextSlide);
-// btnLeft.addEventListener("click", prevSlide);
-
-// dotContainer.addEventListener("click", function (e) {
-//   goToSlide(e.target.classList[1] - 1);
-// });
-// setInterval(nextSlide, 10000);
-
 let currentSlide = 0;
 
 function showSlide(index) {
